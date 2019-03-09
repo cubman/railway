ac4ba4dff616416b36d97838baa2fffeded1b7ae
cubman <tolik.abramoff@yandex.ru>
2019-03-09 16:25:41 +0300

    Обновлена версия спринга, добавлен Spring boot

--- railway-logcon/src/main/java/ru/dstu/railway/logcon/Control.java
+++ railway-logcon/src/main/java/ru/dstu/railway/logcon/Control.java
@@ -1,5 +1,18 @@
 package ru.dstu.railway.logcon;
 
-public class Control {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@SpringBootApplication
+public class Control extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Control.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Control.class, args);
+    }
 }
