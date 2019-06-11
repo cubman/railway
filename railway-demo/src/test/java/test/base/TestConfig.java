fd10a30b2157f618ba144f5d093a3787d1302eb0
Анатолий <tolik.abramoff@yandex.ru>
2019-06-11 22:31:26 +0300

    Исправил поднятие контекста теста

--- railway-demo/src/test/java/test/base/TestConfig.java
+++ railway-demo/src/test/java/test/base/TestConfig.java
@@ -2,6 +2,9 @@ package test.base;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 import ru.dstu.railway.api.state.IStateSender;
 
 @Configuration
@@ -9,5 +12,14 @@ public class TestConfig {
     @Bean
     IStateSender stateSender() {
         return org.mockito.Mockito.mock(IStateSender.class);
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+        JpaTransactionManager transactionManager
+                = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(
+                entityManagerFactory.getObject());
+        return transactionManager;
     }
 }
